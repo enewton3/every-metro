6a80219a7279566c1473f83b2a9d7864b63e9e91
Evyn Newton <newton.evyn@gmail.com>
2020-12-07 12:55:35 -0500

    added some social icons

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -4,12 +4,20 @@ export default function Foot() {
   return (
     <div className="footer">
       <div>
-        <img className="socialIcon" src="" alt="facebook icon" />
+        <img
+          className="socialIcon"
+          src="../images/social/fb.png"
+          alt="facebook icon"
+        />
         <p>
           Built for General Assembly Software Engineering Immersive <br />
           Copyright Evyn Newton 2020
         </p>
-        <img className="socialIcon" src="" alt="instagram icon" />
+        <img
+          className="socialIcon"
+          src="../images/social/ig.png"
+          alt="instagram icon"
+        />
       </div>
     </div>
   );
