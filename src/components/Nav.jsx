44ebd6b6fb38f34a92675187bccacd3b68519996
Evyn Newton <newton.evyn@gmail.com>
2020-12-09 12:57:15 -0500

    added a hamburger menu for smaller screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,34 +1,93 @@
+import { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Nav.css";
+import hamburger from "../images/hamburgericon.png";
 
-export default function Nav() {
-  return (
-    <div className="nav">
-      <Link className="nav-title" to="/">
-        <h2 className="nav-title">Every Metro in the World</h2>
-      </Link>
-      <ul className="link-list">
-        <li>
-          <Link className="nav-link" to="/search">
-            Search for a Metro System
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/tracker">
-            Track Every Metro
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/search">
-            Contribute Data
-          </Link>
-        </li>
-        <li>
-          <Link className="nav-link" to="/about">
-            About
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+//window resizing events help from https://www.jsdiaries.com/how-to-get-the-width-of-the-window-in-react-js/
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      burgerToggle: false,
+      windowWidth: window.innerWidth,
+    };
+  }
+
+  handleResize() {
+    this.setState({ windowWidth: window.innerWidth });
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <Link className="nav-title" to="/">
+          <h2 className="nav-title">Every Metro in the World</h2>
+        </Link>
+        <ul
+          className="link-list"
+          style={
+            this.state.burgerToggle ? { display: "flex" } : { display: "none" }
+          }
+        >
+          <li>
+            <Link className="nav-link" to="/search">
+              Search for a Metro System
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/tracker">
+              Track Every Metro
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/search">
+              Contribute Data
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+        <img
+          className="hamburger"
+          src={hamburger}
+          alt="hamburger menu"
+          onClick={() =>
+            this.setState(
+              this.state.burgerToggle
+                ? { burgerToggle: false }
+                : { burgerToggle: true }
+            )
+          }
+        ></img>
+      </div>
+    );
+  }
 }
+
+// export default function Nav() {
+//   const [burgerToggle, setBurgerToggle] = useState(false);
+//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+//   window.addEventListener("resize", setWindowWidth(window.innerWidth));
+
+//   useEffect(() => {
+//     console.log(windowWidth);
+//     console.log(window.innerWidth);
+//   }, [burgerToggle]);
+
+//   );
+// }
+
+export default Nav;
