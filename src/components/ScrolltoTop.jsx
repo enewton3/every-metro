1796ff46777f401bd11a653fc84a78209167977b
Evyn Newton <newton.evyn@gmail.com>
2020-12-09 15:35:59 -0500

    adding more stylings, trying a scroll to top thing

---
+++ src/components/ScrolltoTop.jsx
@@ -0,0 +1,13 @@
+//from react router docs https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
