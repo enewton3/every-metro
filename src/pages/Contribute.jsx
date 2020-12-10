f0aaa4ad86a474a64e5e5ca5f252bf1c74e68095
Evyn Newton <newton.evyn@gmail.com>
2020-12-10 18:48:48 -0500

    contribute working

--- src/pages/Contribute.jsx
+++ src/pages/Contribute.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { baseURL, config } from "../services";
+import "./Contribute.css";
 
 //gets ID of linked system from params,
 //pulls the data to pre fill the form
@@ -9,36 +10,65 @@ import { baseURL, config } from "../services";
 //pushes edits to the contribute table
 
 export default function Contribute() {
-  const [system, setSystem] = useState({});
   const params = useParams();
+  const [system, setSystem] = useState({
+    fields: {
+      City: "City",
+      Country: "Country",
+      Region: "Region",
+      Name: "Name",
+      OperatedBy: "Operated By",
+      YearOpened: "Year Opened",
+      NumOfLines: "Num Of Lines",
+      Stations: "Stations",
+      SystemLength: "System Length",
+      AnnualRidershipInMillions: "Annual Ridership In Millions",
+      WikiPage: "Wiki Page URL",
+      Modes: "List Public Transit Modes",
+      LightRailTrams: "Associated Light Rail systems",
+      TransitMapUrl: "Transit Map URL",
+      RollingStock: "Rolling Stock Info URL",
+      PaymentType: "List accepted Payment Types",
+      MetroCardName: "Metro Card Name",
+      TransitMuseum: "Transit Museum?",
+      Notes: "Any Notes?",
+      Logo: "Operator Logo URL",
+      OperatorWebsite: "Operator Website URL",
+      Image: "Link to a related system image",
+      Metro_Systems: `${params.id}`,
+    },
+  });
 
   const fields = {
-    City: "City",
-    Country: "Country",
-    Region: "Region",
-    Name: "Name",
-    OperatedBy: "Operated By",
-    YearOpened: "Year Opened",
-    NumOfLines: "Num Of Lines",
-    Stations: "Stations",
-    SystemLength: "System Length",
-    AnnualRidershipInMillions: "Annual Ridership In Millions",
-    WikiPage: "Wiki Page URL",
-    Modes: null,
-    //is there a way to get options from airtable?
-    //is there a way to get an object of fields from airtable?
-    //I have all this in system details technically, do I need to do this again?
-    //How do I now use this to create a form?
-    LightRailTrams: "Associated Light Rail systems",
-    TransitMapUrl: "Transit Map URL",
-    RollingStock: "Rolling Stock Info URL",
-    PaymentType: null,
-    MetroCardName: "Metro Card Name",
-    TransitMuseum: false,
-    Notes: "Any other Notes?",
-    Logo: "Operator Logo URL",
-    OperatorWebsite: "Operator Website URL",
-    Metro_Systems: null,
+    City: `${system.fields.City}`,
+    Country: `${system.fields.Country}`,
+    Region: `${system.fields.Region}`,
+    Name: `${system.fields.Name}`,
+    OperatedBy: `${system.fields.OperatedBy}`,
+    YearOpened: `${system.fields.YearOpened}`,
+    NumOfLines: `${system.fields.NumOfLines}`,
+    Stations: `${system.fields.Stations}`,
+    SystemLength: `${system.fields.SystemLength}`,
+    AnnualRidershipInMillions: `${system.fields.AnnualRidershipInMillions}`,
+    WikiPage: `${system.fields.WikiPage}`,
+    Modes: `${system.fields.Modes}`,
+    LightRailTrams: `${system.fields.LightRailTrams}`,
+    TransitMapUrl: `${system.fields.TransitMapUrl}`,
+    RollingStock: `${system.fields.RollingStock}`,
+    PaymentType: `${system.fields.PaymentType}`,
+    MetroCardName: `${system.fields.MetroCardName}`,
+    TransitMuseum: `${system.fields.TransitMuseum}`,
+    Notes: `${system.fields.Notes}`,
+    Logo: `${system.fields.Logo}`,
+    OperatorWebsite: `${system.fields.OperatorWebsite}`,
+    Image: `${system.fields.Image}`,
+    Metro_Systems: [`${params.id}`],
+  };
+
+  const catchNonTextAndExtra = (obj) => {
+    //go through the objects keys and make sure they are what we want to be sending
+    //then go through the objects values and make sure they are all strings
+    //return a new object
   };
 
   useEffect(() => {
@@ -46,16 +76,13 @@ export default function Contribute() {
       let recordURL = `${baseURL}/Metro_Systems/${params.id}`;
       try {
         let response = await axios.get(recordURL, config);
-        console.log(response);
         setSystem({
           ...response.data,
           fields: {
-            ...fields,
+            ...system.fields,
             ...response.data.fields,
           },
         });
-        console.log(fields);
-        console.log(system);
       } catch (error) {
         console.log(error);
       }
@@ -73,20 +100,24 @@ export default function Contribute() {
     }
   };
 
-  const systemKeysAsArray = Object.keys(fields);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setSystem({
+      fields: {
+        ...system.fields,
+        [name]: value,
+      },
+    });
+  };
 
-  console.log(systemKeysAsArray);
-  const getBetterKeyNames = systemKeysAsArray.map((key) => {
-    // Thanks Soleil for the following reduce!
-    // take each character of the key and let's do something with it
+  // Thanks Soleil for the following reduce!
+  const systemKeysAsArray = Object.keys(system.fields);
+  const systemValuesAsArray = Object.values(system.fields);
+  const betterKeyNames = systemKeysAsArray.map((key) => {
     const words = key.split("").reduce((a, cha, i) => {
-      // if the current letter is a capital letter, it signifies the beginning of a word
       if (cha === cha.toUpperCase()) {
-        // to create a new word, we add a new element at the end of the array to append our lowercase letters to
         a.push(cha);
-        // if the current key is a lowercase letter, it is just part of a word
       } else {
-        // so add it to the current word
         a[a.length - 1] += cha;
       }
       return a;
@@ -94,15 +125,41 @@ export default function Contribute() {
     let detailName = words.join(" ");
     return detailName;
   });
-
+  console.log(system);
+  console.log(system.fields);
+  console.log(systemValuesAsArray);
   return (
     <div>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        {/* {system.fields.map((item) => (
-          <input />
-        ))} */}
-        <button type="submit">Submit</button>
-      </form>
+      <div className="contribute">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          {systemKeysAsArray.map((item, index) => {
+            if (
+              item === "Metro_Systems" ||
+              item === "Visited" ||
+              item === "TransitMap" ||
+              item === "LightRailTrams" ||
+              item === "MetroCardImage" ||
+              item === "logo" ||
+              item === "reviews"
+            ) {
+              return null;
+            } else {
+              return (
+                <div key={`${item}`} className="label-input">
+                  <label htmlFor={`${item}`}>{betterKeyNames[index]}: </label>
+                  <input
+                    id={item}
+                    name={item}
+                    value={systemValuesAsArray[index]}
+                    onChange={(e) => handleInputChange(e)}
+                  />
+                </div>
+              );
+            }
+          })}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
